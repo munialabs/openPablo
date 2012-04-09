3daeaee0bc0b8fd752897f785b93932697bd32f4
Aydin Demircioglu <githubMS@cloned.de>
2012-04-10 01:24:40 +0200

    remove bin/

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,8 @@
 #include <QDir>
 #include <QFile>
 #include <QDataStream>
+#include <QTextStream>
+
 
  #include <magic.h>
 
@@ -83,8 +85,38 @@ using namespace openPablo;
 
 
 
+void customMessageHandler(QtMsgType type, const char *msg)
+{
+	QString txt;
+	switch (type) {
+	case QtDebugMsg:
+		txt = QString("Debug: %1").arg(msg);
+		break;
+
+	case QtWarningMsg:
+		txt = QString("Warning: %1").arg(msg);
+	break;
+	case QtCriticalMsg:
+		txt = QString("Critical: %1").arg(msg);
+	break;
+	case QtFatalMsg:
+		txt = QString("Fatal: %1").arg(msg);
+		abort();
+		break;
+	}
+
+	QFile outFile("/tmp/debuglog.txt");
+	outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+	QTextStream ts(&outFile);
+	ts << txt << endl;
+}
+
+
+
 int main ( int argc, char **argv )
 {
+	// some logging initialization
+	qInstallMsgHandler(customMessageHandler);
 
 //    rtengine::Settings mySettings;
 //    rtengine::init (&s, ".");
